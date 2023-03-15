bc932adccedb16c6a31970ed67fc7f4310cfa7a4
Zhu Jiaying <jiaying.22@intl.zju.edu.cn>
2023-03-15 07:36:15 +0100

    breed

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,7 @@
+n=2 #start with two habbit
+gener=1 #first generation
+while n<=100:
+  n=n*2 #each pair can produce two new rabbits
+  gener=gener+1 
+print ("Starting at the"+str(gener)+" generation, over 100 rabbits have been born")
+
