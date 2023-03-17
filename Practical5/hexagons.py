e59effc798a549bb333a0896260c52c7e7cef353
Zhu Jiaying <jiaying.22@intl.zju.edu.cn>
2023-03-17 09:36:03 +0100

    hexagons

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,6 +1,6 @@
-n=1
-while n<=5:
-  h=2*n*(2*n-1)/2
+n=1 #calculate the first hexagonan
+while n<=5: #display the first five values
+  h=2*n*(2*n-1)/2 #the formula for the nth hexagonal number
   print (h)  
-  n+=1
+  n+=1 #move to next hexagonan
 
