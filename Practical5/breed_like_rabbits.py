ce37fc3d884610c76b97b95e216a0cd9cfeb09d5
Zhu Jiaying <jiaying.22@intl.zju.edu.cn>
2023-03-17 09:39:03 +0100

    breed

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,7 +1,7 @@
 n=2 #start with two habbit
 gener=1 #first generation
-while n<=100:
+while n<=100: #find the first generation when more than 100 rabbits have been born
   n=n*2 #each pair can produce two new rabbits
-  gener=gener+1 
+  gener=gener+1
 print ("Starting at the"+str(gener)+" generation, over 100 rabbits have been born")
 
