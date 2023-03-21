078b8c23a6c878ac4b70aecf6f50f9da542b2844
Zhu Jiaying <jiaying.22@intl.zju.edu.cn>
2023-03-21 04:01:53 +0100

    movie

---
+++ Practical6/movie_genre.py
@@ -0,0 +1,11 @@
+movie_genre={'Comedy':73, 'Action':42, 'Romance':38, 'Fantasy':28, 'Science-fiction':22, 'Horror':19, 'Crime':18, 'Documentary':12, 'History':8, 'War':7} # crate and print a dictionary to record the data
+
+
+import matplotlib.pyplot as plt #draw a pie chart
+labels = 'Comedy', 'Action', 'Romance', 'Fantasy', 'Science-fiction', 'Horro', 'Crime', 'Documentary', 'History', 'War' #enter all movie types as data tags
+sizes = [73, 42, 28, 28, 22, 19, 18, 12, 8, 7] #enter all the data
+explode = (0,0,0,0,0.1,0,0,0,0,0) #set the highlight module offset
+plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',
+        shadow=False, startangle=90) #set the pie properties and draw the image
+plt.axis('equal') #display images
+plt.show() #save and display
